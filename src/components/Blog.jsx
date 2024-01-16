4a1c73653b046bb3967ccbc56bc529094540325f
Iisakki Säkkinen <esakkinen@topcon.com>
2024-01-16 19:53:19 +0200

    blogilistan frontend step7 valmis

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -1,7 +1,49 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from "react"
+
+const Blog = ({ blog }) => {
+
+  const style = {
+    border: "1px solid black",
+    borderRadius: "5px",
+    margin: "5px 0",
+    padding: "10px",
+    backgroundColor: "#f2f2f2",
+    marginBottom: "10px",
+  }
+
+  const buttonStyle = {
+    backgroundColor: "#4CAF50",
+    border: "none",
+    color: "white",
+    padding: "5px 10px",
+    textAlign: "center",
+    textDecoration: "none",
+    display: "inline-block",
+    fontSize: "16px",
+    borderRadius: "5px",
+  }
+
+  const [visible, setVisible] = useState(false)
+
+  const toggleVisibility = () => setVisible(!visible)
+
+  return (
+    <div style={style}>
+      {blog.title} {blog.author}
+      <button style={{...buttonStyle, marginLeft: '5px', marginRight: '5px'}} onClick={toggleVisibility}>{visible ? "hide" : "view"}</button>
+
+      {visible && (
+        <div style={style}>
+          <div><strong>URL:</strong> {blog.url}</div>
+          <div>
+            <strong>Likes:</strong> {blog.likes}
+          </div>
+          <div><strong>Author:</strong> {blog.author}</div>
+          <button style={buttonStyle}>like</button>
+        </div>
+      )}
+    </div>  
+  )
+}
 
 export default Blog
