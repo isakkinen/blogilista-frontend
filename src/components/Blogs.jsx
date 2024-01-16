37406e872ddf88b52756bf3ad3f3c501602c8363
Iisakki Säkkinen <esakkinen@topcon.com>
2024-01-16 20:20:34 +0200

    blogilistan frontend step10 valmis

--- src/components/Blogs.jsx
+++ src/components/Blogs.jsx
@@ -1,7 +1,7 @@
 import Blog from './Blog'
 
 const Blogs = ({blogs, handleLike}) => (
-    <div>{blogs.map(blog => <Blog key={blog.id} blog={blog} handleLike={handleLike} />)}</div>
+    <div>{blogs.sort((a, b) => b.likes - a.likes).map(blog => <Blog key={blog.id} blog={blog} handleLike={handleLike} />)}</div>
 )
 
 export default Blogs
