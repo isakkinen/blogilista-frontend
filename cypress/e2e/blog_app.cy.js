5aba95dcc45bc520de0efaaf213a63080189ff74
Iisakki Säkkinen <esakkinen@topcon.com>
2024-01-17 14:14:52 +0200

    blogilistan end to end testit step2 valmis

--- cypress/e2e/blog_app.cy.js
+++ cypress/e2e/blog_app.cy.js
@@ -4,24 +4,35 @@ describe('Blog app', () => {
         username: 'tester',
         password: 'salainen'
     }
+    beforeEach(function() {
+        cy.request('POST', 'http://localhost:3003/api/testing/reset')
+        cy.request('POST', 'http://localhost:3003/api/users', user)
+        cy.visit('http://localhost:5173')
+    })
     describe('When first entering page', function() {
-        beforeEach(function() {
-            cy.request('POST', 'http://localhost:3003/api/testing/reset')
-            cy.request('POST', 'http://localhost:3003/api/users', user)
-            cy.visit('http://localhost:5173')
-        })
 
-        it('front page can be opened', function() {
+        it('Login form is shown', function() {
             cy.contains('Login')
         })
 
-        it('login proceeds to blog listing', function() {
-            cy.contains('Login')
-            cy.get('#username').type(user.username)
-            cy.get('#password').type(user.password)
-            cy.contains('login').click()
+        describe('Login', function() {
+            it('fails with wrong password', function() {
+                cy.contains('Login')
+                cy.get('#username').type(user.username)
+                cy.get('#password').type('wrong')
+                cy.contains('login').click()
 
-            cy.contains('Iisakki Säkkinen logged in')
+                cy.contains('invalid username or password')
+            })
+
+            it('proceeds to blog listing when correct credentials are given', function() {
+                cy.contains('Login')
+                cy.get('#username').type(user.username)
+                cy.get('#password').type(user.password)
+                cy.contains('login').click()
+
+                cy.contains('Iisakki Säkkinen logged in')
+            })
         })
     })
 
